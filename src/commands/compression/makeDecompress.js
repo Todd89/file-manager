6ed1ebfa4729cd6eb5728ad9b1ad951afa2c9988
Aleksandr Golamonov <alex-89fish@tut.by>
2022-06-12 21:54:20 +0300

    fix

--- src/commands/compression/makeDecompress.js
+++ src/commands/compression/makeDecompress.js
@@ -2,7 +2,7 @@ import  zlib from 'zlib';
 import fs from 'fs';
 
 export const makeDecompress = async (command) => {
-  const gzip = zlib.createBrotliCompress();
+  const gzip = zlib.createBrotliDecompress();
 
   const chunkStringified = command.toString().trim();
   const pathTwoVar = chunkStringified.slice(10);
@@ -15,12 +15,13 @@ export const makeDecompress = async (command) => {
     new_path = new_path.split('')[1] === ":" ? `${new_path}/${file}` : `${process.cwd()}/${new_path}/${file}`;
     new_path = new_path.substring(0, new_path.length - 3);
 
+    console.log(new_path);
 
     const  read = fs.createReadStream(`${process.cwd()}/${file}`);
     const  write = fs.createWriteStream(`${new_path}`);
 
-         read.pipe(gzip).on('error', err => {console.log("Operation failed")}).pipe(write)
-         .on('error', err => {console.log("Operation failed")})
+         read.pipe(gzip).on('error', err => {console.log("Operation failed1")}).pipe(write)
+         .on('error', err => {console.log("Operation failed2")})
          .on('finish', (err) => {
           console.log("Decompress was successful")
             });
